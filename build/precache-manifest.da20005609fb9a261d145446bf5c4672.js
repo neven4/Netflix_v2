b6b87053ea476d86000c1c91bff5e8f82f8deb67
unknown <kazakovkirill1@gmail.com>
2019-03-12 00:50:22 +0300

    heroku deploy

---
+++ build/precache-manifest.da20005609fb9a261d145446bf5c4672.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "0c92d60caf26f15e20d77a8b1b7e2b07",
+    "url": "/static/media/pexels-photo-91227.0c92d60c.jpeg"
+  },
+  {
+    "revision": "c53e3cdc982b2cd3c8c4",
+    "url": "/static/css/main.b105240e.chunk.css"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "b7c45de6991615838f63",
+    "url": "/static/js/2.2644c515.chunk.js"
+  },
+  {
+    "revision": "59b0f29fc702b1e245bc3de3c319aae1",
+    "url": "/static/media/tmdbLogo.59b0f29f.svg"
+  },
+  {
+    "revision": "671d88ad24414c16f98b8101f05beb98",
+    "url": "/static/media/noa.671d88ad.jpg"
+  },
+  {
+    "revision": "c53e3cdc982b2cd3c8c4",
+    "url": "/static/js/main.36c9cd12.chunk.js"
+  },
+  {
+    "revision": "18b2429ba6e7179daeec5438639ab65f",
+    "url": "/static/media/RobotoRegular.18b2429b.woff"
+  },
+  {
+    "revision": "af01b5037ff63cf05210745f4c248269",
+    "url": "/static/media/RobotoBold.af01b503.woff"
+  },
+  {
+    "revision": "42386a032cd63442e3c8b41b6c8dbf06",
+    "url": "/static/media/SourceSansProBold.42386a03.woff"
+  },
+  {
+    "revision": "a2a7fa73e5fe01b756a05d98b6a851b4",
+    "url": "/static/media/SourceSansProRegular.a2a7fa73.woff"
+  },
+  {
+    "revision": "69b787c9775c9bd0ac27f3fc3fa19223",
+    "url": "/static/media/SourceSansProLight.69b787c9.woff"
+  },
+  {
+    "revision": "549a7c416f9fd2ff33a05073fa7c26f1",
+    "url": "/index.html"
+  }
+];
